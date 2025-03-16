a6a5460ba493c3cc1984d60665adf5178e82b4e6
Mauricio Cardoso <dev.mauriciocardoso@gmail.com>
2025-03-16 19:56:10 -0400

    feat(seed): add seed data

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (User::count() == 0) {
+            User::factory(25)->create();
+        }
+    }
+}
