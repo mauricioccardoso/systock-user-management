c1259181d823c231b08245b744cf2940670c35f1
Mauricio Cardoso <dev.mauriciocardoso@gmail.com>
2025-03-16 19:03:32 -0400

    feat(user): add user creation

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Logger;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        DB::beginTransaction();
+        try {
+            $user = User::create($request->except(['password_confirmation']));
+            DB::commit();
+
+            return new UserResource($user);
+
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            $error = 'Unable to create a new user.';
+            Logger::log($th, $error);
+
+            return response()->json(["error" => $th], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', function() {
-    return response()->json(["login" => true]);
-});
+Route::apiResource('user', UserController::class);
