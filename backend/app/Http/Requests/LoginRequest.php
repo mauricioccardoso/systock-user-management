4b83b29afd798a549a1d73d87fada05f411a933b
Mauricio Cardoso <dev.mauriciocardoso@gmail.com>
2025-03-16 21:29:54 -0400

    feat(auth): add authentication

---
+++ backend/app/Http/Requests/LoginRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => [
+                'required',
+                'email',
+            ],
+            'password' => [
+                'required',
+                Password::min(8)
+            ],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.email' => 'O email deve ser um endereço de email válido.',
+            'email.unique' => 'Este email já está registrado.',
+
+            '*.required' => 'O campo :attribute é obrigatório.',
+            '*.min' => 'O campo :attribute deve ter pelo menos :min caracteres.',
+        ];
+    }
+}
